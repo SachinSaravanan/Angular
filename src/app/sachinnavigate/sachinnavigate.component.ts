a09a07b5d3aecf5637aabe926eb5ade217f934f2
unknown <ssachinsachin113@gmail.com>
2023-11-02 10:26:29 +0530

    sachin pushing

--- src/app/sachinnavigate/sachinnavigate.component.ts
+++ src/app/sachinnavigate/sachinnavigate.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./sachinnavigate.component.scss']
 })
 export class SachinnavigateComponent {
+  sachinelement = false;
+  onclick(){
+    this.sachinelement=!this.sachinelement;
+  }
 
 }
